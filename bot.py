8656d54cc4fe390c203955971649bc4f7b079af3
Ajay Bati <ajaybati@gmail.com>
2020-05-26 03:42:14 -0700

    updating security

--- bot.py
+++ bot.py
@@ -1,33 +1,32 @@
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.common.action_chains import ActionChains
-
-class InstaBot:
-    def __init__(self, username, pw):
-        self.driver = webdriver.Chrome()
-
-        self.driver.get("https://www.messenger.com/")
-
-
-
-
-        self.driver.find_element_by_xpath("//input[@name=\"email\"]").send_keys(username)
-        self.driver.find_element_by_xpath("//input[@name=\"pass\"]").send_keys(pw)
-
-        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
-        # try:
-        #
-        #     for x in range(2000):
-        #         self.driver.find_element_by_xpath('//a[@title="Send a Like"]').click()
-        #         sleep(0.1)
-        # except Exception as e:
-        #     pass
-        self.driver.find_element_by_xpath("//*[@data-editor]").click()
-        actions = ActionChains(self.driver)
-        actions.send_keys('I LOVE ME SOME VERMA')
-        actions.perform()
-        print("success")
-
-        sleep(2000)
-
-InstaBot('5109531173', 'Ajay@0819')
+# from selenium import webdriver
+# from time import sleep
+# from selenium.webdriver.common.action_chains import ActionChains
+#
+# class InstaBot:
+#     def __init__(self, username, pw):
+#         self.driver = webdriver.Chrome()
+#
+#         self.driver.get("https://www.messenger.com/")
+#
+#
+#
+#
+#         self.driver.find_element_by_xpath("//input[@name=\"email\"]").send_keys(username)
+#         self.driver.find_element_by_xpath("//input[@name=\"pass\"]").send_keys(pw)
+#
+#         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
+#         # try:
+#         #
+#         #     for x in range(2000):
+#         #         self.driver.find_element_by_xpath('//a[@title="Send a Like"]').click()
+#         #         sleep(0.1)
+#         # except Exception as e:
+#         #     pass
+#         self.driver.find_element_by_xpath("//*[@data-editor]").click()
+#         actions = ActionChains(self.driver)
+#         actions.send_keys('I LOVE ME SOME VERMA')
+#         actions.perform()
+#         print("success")
+#
+#         sleep(2000)
+#
